72ce2c801be43798f057b04adff9b3f8d992a691
Andru <andru.mao94@gmail.com>
2019-03-26 11:43:29 +0200

    [Andru] Highlight current route in sidebar

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -79,32 +79,20 @@
             <!-- Navigation -->
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('home') }}">
-                        <i class="ni ni-tv-2 text-primary"></i> {{ __('Dashboard') }}
+                    <a class="nav-link @if (Route::currentRouteName() === 'home') text-primary @endif" href="{{ route('home') }}">
+                        <i class="ni ni-tv-2"></i> {{ __('Dashboard') }}
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-examples">
-                        <i class="fab fa-laravel" style="color: #f4645f;"></i>
-                        <span class="nav-link-text" style="color: #f4645f;">{{ __('Laravel Examples') }}</span>
+                    <a class="nav-link @if (Route::currentRouteName() === 'profile.edit') text-primary @endif" href="{{ route('profile.edit') }}">
+                        <i class="ni ni-single-02"></i> {{ __('Profile') }}
                     </a>
-
-                    <div class="collapse show" id="navbar-examples">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('profile.edit') }}">
-                                    {{ __('User profile') }}
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('user.index') }}">
-                                    {{ __('User Management') }}
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
                 </li>
-
+                <li class="nav-item">
+                    <a class="nav-link @if (Route::currentRouteName() === 'user.index') text-primary @endif" href="{{ route('user.index') }}">
+                        <i class="ni ni-badge"></i> {{ __('User Management') }}
+                    </a>
+                </li>
                 
             </ul>
             <!-- Divider -->
